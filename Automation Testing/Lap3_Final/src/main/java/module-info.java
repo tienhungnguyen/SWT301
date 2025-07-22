c61b29f9ed4c6c62f476d3d015cf411030662abb
tienhungnguyen <tienhung1201@gmail.com>
2025-07-22 10:02:42 +0700

    delete

--- Automation Testing/Lap3_Final/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module nguyentienhung.example.lap3_part2 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens nguyentienhung.example.lap3_part2 to javafx.fxml;
-    exports nguyentienhung.example.lap3_part2;
-    requires java.logging;
-}
--- Automation Testing/Lap3_Final/src/main/java/nguyentienhung/example/lap3_part2/InterfaceNamingInconsistencyExample.java
+++
@@ -1,5 +0,0 @@
-package nguyentienhung.example.lap3_part2;
-
-interface LoginHandler {
-    boolean login(String username, String password);
-}
