74fac90a8c025205c281c23d1f0afcb1955328a7
tienhungnguyen <tienhung1201@gmail.com>
2025-07-22 10:03:46 +0700

    delete

--- Automation Testing/Lap2_UnitTest_SignUp/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module tienhung.service.lap2_unittest_signup {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens tienhung.service.service to javafx.fxml;
-    exports tienhung.service.service;
-}
